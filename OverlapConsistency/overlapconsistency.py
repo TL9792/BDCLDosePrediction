78022e8fcf88614964a63c7193d4661f46c2be08
TL9792 <76771042+TL9792@users.noreply.github.com>
2022-10-10 12:15:46 +0800

    Create overlapconsistency.py

---
+++ OverlapConsistency/overlapconsistency.py
@@ -0,0 +1,18 @@
+import torch
+
+def consistency(pd,msk):
+    " pd is the predicted beam voters, and msk represents beam masks"
+    
+    sum_pd = torch.sum(pd, dim=1).unsqueeze(1)
+    sum_msk = torch.sum(msk, dim=1).unsqueeze(1)
+
+    sum_msk[sum_msk < 2] = 1   # (2,1,128,128,128)
+    overlap_sum_msk = sum_msk > 1
+    overlap_msk = overlap_sum_msk * msk
+    avg = (sum_pd*overlap_sum_msk) / sum_msk
+    diff = torch.abs(pd * overlap_msk - avg)
+    loss = (torch.sum(diff) / torch.sum(overlap_msk))
+
+    return loss
+    
+  
