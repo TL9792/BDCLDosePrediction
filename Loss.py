87e0ea16baef19d265dc854e490873c26eea6efe
TL9792 <76771042+TL9792@users.noreply.github.com>
2022-10-11 10:19:44 +0800

    Update Loss.py

--- Loss.py
+++ Loss.py
@@ -22,9 +22,11 @@ def loss(pd_dose,gt_dose, dose_mask, roi, spacing, coarse_dose, gtimg, whole_mas
     "training loss function"
     reimg = reconstruct(pd_dose, coarse_dose)
     doseloss = mae_loss(pd_dose, gt_dose, dose_mask)
-    reconsloss = 0.5*mae_loss(reimg, gtimg, whole_mask)
-    consistloss = 0.1*consistloss(pd_dose,dose_mask)
+    Lr = 0.5*mae_loss(reimg, gtimg, whole_mask)
+    Lcs = 0.1*consistloss(pd_dose,dose_mask)
     cdvhloss = 0.1*dvh_loss_c(reimg,gtimg, whole_mask)
     fdvhloss = 0.5*dvh_loss_v(reimg, gtimg, roi, spacing)
-    gradloss = 0.5*gradient_loss(pd_dose, gt_dose, dose_mask)
-    return (doseloss+reconsloss+consistloss+cdvhloss+fdvhloss+gradloss),doseloss,reconsloss,consistloss,cdvhloss,fdvhloss,gradloss,reimg
+    Le = 0.5*gradient_loss(pd_dose, gt_dose, dose_mask)
+    return (doseloss+Lr+Lcs+cdvhloss+fdvhloss+Le),reimg
+
+
