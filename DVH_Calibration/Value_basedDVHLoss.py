68a530c0808e9243451c3b788ff7167815af142d
TL9792 <76771042+TL9792@users.noreply.github.com>
2022-10-13 22:01:22 +0800

    Update Value_basedDVHLoss.py

--- DVH_Calibration/Value_basedDVHLoss.py
+++ DVH_Calibration/Value_basedDVHLoss.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.nn.modules import padding
 from torch.utils.data.dataset import ConcatDataset
 
+
 def dvh_loss_v(pd, gt, masks):
     """
     masks should have 10 roi masks including
@@ -13,7 +14,7 @@ def dvh_loss_v(pd, gt, masks):
             'Esophagus', 'Larynx', 'Mandible'], targets=['PTV56', 'PTV63', 'PTV70']
     """
 
-    # initial of the diff
+    # initialize the diff
     Loss = 0
     n = 0
     max_w = 0
@@ -32,35 +33,27 @@ def dvh_loss_v(pd, gt, masks):
             gt_vec = gt[batch, 0, ...].flatten()
             mask_vec = masks[batch, roi, ...].flatten().type(torch.bool)
 
-            # if mask has no value eg. all zero, then next roi
+            # if mask has no value e.g. all zero, then next roi
             if mask_vec.nonzero(as_tuple = False).numel() == 0:
                 continue
                 
-            # get mask idx and mask val in pred
+            # get mask val in pred
             pd_nonzero_val = pd_vec[mask_vec]
-            pd_nonzero_idx = mask_vec.nonzero(as_tuple = False)
 
             # sort the mask val
             pd_sort_val = pd_nonzero_val.topk(pd_nonzero_val.numel(), dim=0).values
-            pd_sort_idx = pd_nonzero_val.topk(pd_nonzero_val.numel(), dim=0).indices
-
-            # get the mask val indx in pred image
-            pd_ori_idx = pd_nonzero_idx[pd_sort_idx]
-
 
             # get mask idx and mask val in gt
             gt_nonzero_val = gt_vec[mask_vec]
-            gt_nonzero_idx = mask_vec.nonzero(as_tuple = False)
 
             # sort the mask val
             gt_sort_val = gt_nonzero_val.topk(gt_nonzero_val.numel(), dim=0).values
-            gt_sort_idx = gt_nonzero_val.topk(gt_nonzero_val.numel(), dim=0).indices
 
-            # get the mask val indx in gt image
-            gt_ori_idx = gt_nonzero_idx[gt_sort_idx]
+            # calculate the value-based DVH loss
+            Loss += (torch.abs(gt_sort_val - pd_sort_val)).sum()
 
-            Loss += ( torch.abs(gt_sort_val - pd_sort_val)).sum()
-
+            # the total voxel nums in ROIs 
             n += pd_sort_val.numel()
 
     return (Loss / (n))
+
