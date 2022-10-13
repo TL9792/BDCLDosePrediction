673cd37737d834d69a2cc2529ea13160320fb6f4
TL9792 <76771042+TL9792@users.noreply.github.com>
2022-10-13 21:15:32 +0800

    Update GradientLoss.py

--- EdgeEnhancement/GradientLoss.py
+++ EdgeEnhancement/GradientLoss.py
@@ -1,8 +1,15 @@
 import torch
+import torch.nn.functional as F 
+
 
 def gradient_loss(pd, gt, mask):
-  "pd is the predicted dose distribution map, gt represents the ground truth and mask represents the dose mask."
-  
+    """
+    pd is the predicted dose distribution map, gt represents the ground truth, and mask represents the dose mask
+    pd: b, 1, 128, 128, 128
+    gt: b, 1, 128, 128, 128
+    mask: b, 1, 128, 128, 128
+    """
+    
     sobelx = torch.tensor([[[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]],
                             [[-2, 0, 2], [-4, 0, 4], [-2, 0, 2]], 
                             [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]]).expand(pd.shape[0], 1, 3, 3, 3).cuda().float()
@@ -12,12 +19,13 @@ def gradient_loss(pd, gt, mask):
     sobelz = torch.tensor([ [[-1, -2, -1], [-2, -4, -2], [-1, -2, -1]], 
                             [[1, 2, 1], [2, 4, 2], [1, 2, 1]], 
                             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]).expand(pd.shape[0], 1, 3, 3, 3).cuda().float()
-    pdx = F.conv3d(pd.float(), torch.cat([sobelx]*9, dim=1), padding=1, stride=1)
-    gtx = F.conv3d(gt.float(), torch.cat([sobelx]*9, dim=1), padding=1, stride=1)
-    pdy = F.conv3d(pd.float(), torch.cat([sobely]*9, dim=1), padding=1, stride=1)
-    gty = F.conv3d(gt.float(), torch.cat([sobely]*9, dim=1), padding=1, stride=1)
-    pdz = F.conv3d(pd.float(), torch.cat([sobelz]*9, dim=1), padding=1, stride=1)
-    gtz = F.conv3d(gt.float(), torch.cat([sobelz]*9, dim=1), padding=1, stride=1)        
+    pdx = F.conv3d(pd.float(), sobelx, padding=1, stride=1)
+    gtx = F.conv3d(gt.float(), sobelx, padding=1, stride=1)
+    pdy = F.conv3d(pd.float(), sobely, padding=1, stride=1)
+    gty = F.conv3d(gt.float(), sobely, padding=1, stride=1)
+    pdz = F.conv3d(pd.float(), sobelz, padding=1, stride=1)
+    gtz = F.conv3d(gt.float(), sobelz, padding=1, stride=1)        
     return ( torch.abs(pdx - gtx).sum() + torch.abs(pdy - gty).sum() + torch.abs(pdz - gtz).sum() ) / mask.sum()
+
   
   
